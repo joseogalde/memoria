f1fd40d33569b855f6a7118f6227e09ca67234fa
joseogalde <jose.ogalde@ing.uchile.cl>
2016-05-17 19:27:28 -0400

    se modifican los scripts y se escriben como funciones. Se utiliza simulink con objetos timeseries para añadir el modelo teorico del payload

---
+++ matlab/payloadLogParser.m
@@ -0,0 +1,83 @@
+function parseMATFileName = payloadLogParser(prefix)
+logsFolderPath = './cutecom/';
+%% Input
+reglInput = 'rand() = ';
+regrInput = ' ';
+logFileName = strcat(logsFolderPath ,prefix, 'input_voltages.txt');
+
+values = [];
+
+fid = fopen(logFileName);
+tline = fgets(fid);
+
+while ischar(tline)
+    if  ~isempty(  strfind( tline, reglInput ) )
+        indexl = strfind( tline, reglInput );
+        indexl = indexl + length( reglInput );
+        tmp = tline ( indexl : end );
+        indexr = strfind( tmp, regrInput );
+        nextValue = str2double( tmp( 1 : indexr - 1 ) );
+        values = [values; nextValue];
+    end
+    tline = fgets(fid);
+end
+
+fclose(fid);
+
+Input.file = logFileName;
+Input.len = length(values);
+Input.counts = values;
+Input.nbits = 16;
+
+%% Output
+numberOfFiles = 10; %freqs executed
+reglOutput = 'dat_set_Payload_Buff(';
+regrOutput = ')';
+regADC =  'adc period = ';
+oversamplingCoeff = 4;
+
+for i = 0 : numberOfFiles - 1
+    logFileName = strcat( logsFolderPath, prefix, 'freq', num2str(i), '.txt' );
+    
+    values = [];
+    adcPeriod = 0;
+    
+    fid = fopen(logFileName);
+    tline = fgets(fid);
+    
+    while ischar(tline)
+        if ~isempty( strfind(tline, regADC) )
+            indexl = strfind( tline, regADC);
+            indexl = indexl + length(regADC);
+            adcPeriod = str2double( tline(indexl: end) );
+            
+        elseif  ~isempty(  strfind(tline, reglOutput) )
+            indexl = strfind(tline, reglOutput);
+            indexl = indexl + length(reglOutput);
+            indexr = strfind(tline, regrOutput);
+            nextValue = str2double( tline(indexl : indexr - 1) );
+            values = [values; nextValue];
+        end
+        tline = fgets(fid);
+    end
+    fclose(fid);
+    
+    Output.file = logFileName;
+    Output.len = length(values);
+    Output.counts = values;
+    Output.nbits = 10;
+    
+    Measure.dac = Input;
+    Measure.adc = Output;
+    Measure.oversamplingCoeff = oversamplingCoeff;
+    Measure.adcPeriod = adcPeriod;
+    Measure.date = prefix;
+    
+    logName = strcat('freq', num2str(i));
+    ExpFisParse.(logName) = Measure;
+end
+
+parseMATFileName = strcat(prefix,'ExpFisParse.mat');
+save(parseMATFileName, 'ExpFisParse');
+
+end
---
+++ matlab/payloadTimeSeriesBuilder.m
@@ -0,0 +1,99 @@
+function timeseriesMATFileName = payloadTimeSeriesBuilder(prefix)
+parseMATFile = strcat(prefix, 'ExpFisParse.mat');
+
+if ~exist(parseMATFile,'file')
+    parseMATFile = payloadLogParser(prefix);
+end
+
+load(parseMATFile);
+names = fieldnames(ExpFisParse);
+
+[m, n, ~, ~] = payloadLinearFit;
+powerFactor = 0.8264;
+
+for i = 1 : 5%length( fieldnames ( ExpFisParse ) )
+    %% load from MAT-file & prepare 
+    MeasureStruct = ExpFisParse.(names{i});
+    
+    ADCregister = MeasureStruct.adcPeriod;
+    deltaTSignal = m * ADCregister + n;
+    oversamplingCoeff = MeasureStruct.oversamplingCoeff;
+    freqSignalHz = 1 / deltaTSignal;
+    
+    disp(strcat(num2str(i-1),' Iniciando freq ', num2str(freqSignalHz), ' Hz'));
+    fsHz = freqSignalHz * oversamplingCoeff;
+    deltaTSampling = 1 / fsHz;
+    
+    bitsDAC = MeasureStruct.dac.nbits;
+    bitsADC = MeasureStruct.adc.nbits;
+    countsDACArray = MeasureStruct.dac.counts;
+    countsADCArray = MeasureStruct.adc.counts;
+    tDAC = deltaTSignal .* (0 : length(countsDACArray) - 1);
+    tADC = deltaTSampling .* (0 : length(countsADCArray) - 1);
+    
+    %% Data timeseries
+    countsDAC = timeseries( countsDACArray, tDAC, 'name', 'dac');
+    countsADC = timeseries( countsADCArray, tADC, 'name', 'adc');
+    countsDAC.DataInfo.Units = 'Counts';
+    countsADC.DataInfo.Units = 'Counts';
+    countsDAC.TimeInfo.Units = 'seconds';
+    countsADC.TimeInfo.Units = 'seconds';
+    countsDAC.DataInfo.Interpolation = tsdata.interpolation('zoh');
+    countsADC.DataInfo.Interpolation = tsdata.interpolation('zoh');
+    
+    countsDAC = resample(countsDAC, tADC);
+    nanRows = find(isnan(countsDAC.Data));
+    countsDAC.Data = nanReplace(countsDAC.Data, nanRows);
+    
+    % translate counts to voltages
+    vin = timeseries(countsDAC.Data, countsDAC.Time, 'name', 'vin');
+    vin.Data = count2voltage(vin.Data, 3.3, 0, bitsDAC);
+    vin.DataInfo.Units = 'V';
+    vin.DataInfo.Interpolation = tsdata.interpolation('zoh');
+    vin.TimeInfo.Units = 'seconds';
+    
+    vout = timeseries(countsADC.Data, countsADC.Time, 'name', 'vout');
+    vout.Data = count2voltage(vout.Data, 3.3, 0, bitsADC);
+    vout.DataInfo.Units = 'V';
+    vout.DataInfo.Interpolation = tsdata.interpolation('zoh');
+    vout.TimeInfo.Units = 'seconds';
+    
+    power = timeseries(  powerFactor.* (vin.Data.*vout.Data), vout.Time, 'name', 'power');
+    power.DataInfo.Units = 'mW';
+    power.TimeInfo.Units = 'seconds';
+    
+    %create timeseries collection
+    tscName = strcat( 'tscData', num2str(i-1) );
+    tscData = tscollection({countsDAC, countsADC, vin, vout, power}, 'Name', tscName);
+    
+    %% Simulation timeseries
+    
+    simVin = tscData.vin;
+    simVin.Name = 'simVin';
+    startTime = simVin.TimeInfo.Start;
+    stopTime = simVin.TimeInfo.End;
+    sampleTime = simVin.TimeInfo.Increment;
+    
+    options = simset('SrcWorkspace','current');
+    sim('payloadModel',[],options)
+    
+    simVout.Name = 'simVout';
+    simVout.DataInfo.Units = 'V';
+    
+    simpower = timeseries( powerFactor .* (simVin.Data.*simVout.Data), simVout.Time, 'name', 'simPower');
+    simpower.DataInfo.Units = 'mW';
+    
+    tscSimulationName = strcat( 'tscSimulation', num2str(i-1) );
+    tscSimulation = tscollection({simVin, simVout, simpower}, 'Name', tscSimulationName);
+    
+    SaveTSData.fsHz = fsHz;
+    SaveTSData.freqSignalHz = freqSignalHz;
+    SaveTSData.tscData = tscData;
+    SaveTSData.tscSimulation = tscSimulation ;
+    ExpFisTimeSeries.(names{i}) = SaveTSData ;
+    
+end
+
+timeseriesMATFileName = strcat(prefix, 'ExpFisTimeSeries.mat');
+save(timeseriesMATFileName,'ExpFisTimeSeries','-v7.3');
+end
