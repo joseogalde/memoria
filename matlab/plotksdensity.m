d337d0e93810913ff793e3cd6421440b25d54794
joseogalde <jose.ogalde@ing.uchile.cl>
2016-05-25 17:32:53 -0400

    script para plotear los histogramas calculados junto con las pdfs fiteadas

--- matlab/plotksdensity.m
+++ matlab/plotksdensity.m
@@ -1,9 +1,8 @@
-clear all;
 close all;
 
-kernelBW = 50;
 kernelResolution = 1000;
 errorsFileName = 'Errors_LessMoreData.mat';
+saveFolder ='./img/distributions/';
 
 date = {'2016_17_05', '2016_18_05'};
 prefix = strcat(date{1},'_');
@@ -14,7 +13,7 @@ prefix = strcat(date{2},'_');
 histogramFileNameMore = strcat(prefix, 'ExpFisHistogram.mat');
 timeseriesFileNameMore = strcat(prefix, 'ExpFisTimeSeries.mat');
 
-if ~exist(errorsFileName,'file')
+% if ~exist(errorsFileName,'file')
     load( histogramFileNameLess );
     load( timeseriesFileNameLess );
     ExpFisHistogramLess = ExpFisHistogram;
@@ -34,16 +33,21 @@ if ~exist(errorsFileName,'file')
     KLVout = zeros(1,length(names));
     KLPower = zeros(1,length(names));
     
-    parfor i = 1: length(names)
+    for i = 1 : length(names)
         %% Load counts and edges
         HistCountsLess = ExpFisHistogramLess.(names{i});
         HistCountsMore = ExpFisHistogramMore.(names{i});
         dataCountsLess = HistCountsLess.data;
+        simCountsLess = HistCountsLess.simulation;
         dataCountsMore = HistCountsMore.data;
+        simCountsMore = HistCountsMore.simulation;
         TSeriesLess = ExpFisTSeriesLess.(names{i});
         TSeriesMore = ExpFisTSeriesMore.(names{i});
         dataTSCLess = TSeriesLess.tscData;
+        simTSCLess = TSeriesLess.tscSimulation;
         dataTSCMore = TSeriesMore.tscData;
+        simTSCMore = TSeriesMore.tscSimulation;
+        
         f(i) = HistCountsLess.freqSignalHz;
         
         %% Vout
@@ -54,6 +58,13 @@ if ~exist(errorsFileName,'file')
         dxLess = (eVoutLess(2) - eVoutLess(1));
         areaVoutL = sum(hVoutLess) * dxLess ;
         
+        hSimVoutLess = simCountsLess.hVout;
+        eSimVoutLess = simCountsLess.eVout;
+        voutSimLess = simTSCLess.simVout.Data;
+        voutSimLess = voutSimLess - mean(voutSimLess);
+        dxSimLess = (eSimVoutLess(2) - eSimVoutLess(1));
+        areaSimVoutL = sum(hSimVoutLess) * dxSimLess ;
+        
         hVoutMore = dataCountsMore.hVout;
         eVoutMore = dataCountsMore.eVout;
         voutDataMore = dataTSCMore.vout.Data;
@@ -61,71 +72,142 @@ if ~exist(errorsFileName,'file')
         dxMore = (eVoutMore(2)-eVoutMore(1));
         areaVoutM = sum(hVoutMore) * dxMore;
         
-        xx = linspace(-1,1, kernelResolution);
+        hSimVoutMore = simCountsMore.hVout;
+        eSimVoutMore = simCountsMore.eVout;
+        voutSimMore = simTSCMore.simVout.Data;
+        voutSimMore = voutSimMore - mean(voutSimMore);
+        dxSimMore = (eSimVoutMore(2) - eSimVoutMore(1));
+        areaSimVoutM = sum(hSimVoutMore) * dxSimMore ;
+        
+        xmin = min([min(eVoutLess),min(eSimVoutLess),min(eVoutMore),...
+            min(eSimVoutMore)]);
+        xmax = max([max(eVoutLess),max(eSimVoutLess),max(eVoutMore), ...
+            max(eSimVoutMore)]);
+        xx = linspace(xmin,xmax, kernelResolution);
         [fVoutL, xiVoutL, bwVoutL] = ksdensity(voutDataLess,xx);
+        [fSimVoutL, xiSimVoutL, bwSimVoutL] = ksdensity(voutSimLess,xx);
         [fVoutM, xiVoutM, bwVoutM] = ksdensity(voutDataMore,xx);
+        [fSimVoutM, xiSimVoutM, bwSimVoutM] = ksdensity(voutSimMore,xx);
         areafVoutL = trapz(xx,fVoutL);
+        areafSimVoutL = trapz(xx,fSimVoutL);
         areafVoutM = trapz(xx,fVoutM);
+        areafSimVoutM = trapz(xx,fSimVoutM);
         fCountsVoutL = (areaVoutL/areafVoutL).*fVoutL;
+        fCountsSimVoutL = (areaSimVoutL/areafSimVoutL).*fSimVoutL;
         fCountsVoutM = (areaVoutM/areafVoutM).*fVoutM;
+        fCountsSimVoutM = (areaSimVoutM/areafSimVoutM).*fSimVoutM;
         pdfVoutL = normalize(fCountsVoutL);
         pdfVoutM = normalize(fCountsVoutM);
-        KLVout(i) = kldiv(xiVoutL, pdfVoutL, pdfVoutM, 'sym');
+        pdfSimVoutL = normalize(fCountsSimVoutL);
+        pdfSimVoutM = normalize(fCountsSimVoutM);
+%         KLVout(i) = kldiv(xiVoutL, pdfVoutL, pdfVoutM, 'sym');
         
         figure('units','normalized','outerposition',[0 0 1 1]);
+        subplot(2,1,1);
         hold on;
         barX = eVoutLess(2:end);
-        barY = [hVoutLess', hVoutMore'];
+        barY = [hVoutLess', hSimVoutLess'];
         bar(barX,barY,'hist');
         plot(xx,fCountsVoutL,'b-');
-        plot(xx, fCountsVoutM,'k-');        
-        legend('Hist Counts','fCountsVoutL','fCountsVoutM');
-        title(strcat('Vout f = ',num2str(f(i)), ' Hz'));
+        plot(xx,fCountsSimVoutL,'k-');
+        xlim([-1.1 1.1]);
+        xlabel('Vout [V]');
+        ylabel('Counts');
+        legend('Hist Data','Hist Simulation','Fitted Hist Data','Fitted Hist Simulation');
+        title(strcat('Smoothing 4000 samples for input signal of',' ',num2str(f(i)), ' ','Hz'));
+        
+        subplot(2,1,2);
+        hold on;
+        barX = eVoutMore(2:end);
+        barY = [hVoutMore', hSimVoutMore'];
+        bar(barX,barY,'hist');
+        plot(xx, fCountsVoutM,'b-');
+        plot(xx, fCountsSimVoutM,'k-');  
+        xlim([-1.1 1.1]);
+        xlabel('Vout [V]');
+        ylabel('Counts');
+        legend('Data Hist','Simulation Hist','Fitted Data Hist','Fitted Simulation Hist');
+        title(strcat('Smoothing 4000 samples for input signal of ',num2str(f(i)), ' Hz'));
+        saveas(gcf,strcat(saveFolder,'ksdensityLess_freq',num2str((i-1)),'.png'));
         
         %% Power
         hPowerLess = dataCountsLess.hPower;
         ePowerLess = dataCountsLess.ePower;
         powerDataLess = dataTSCLess.power.Data;
         powerDataLess = powerDataLess - mean(powerDataLess);
-        areaPowerL = sum(hPowerLess) * (ePowerLess(2) -ePowerLess(1));
+        dxLess = (ePowerLess(2) - ePowerLess(1));
+        areaPowerL = sum(hPowerLess) * dxLess ;
+        
+        hSimPowerLess = simCountsLess.hPower;
+        eSimPowerLess = simCountsLess.ePower;
+        powerSimLess = simTSCLess.simPower.Data;
+        powerSimLess = powerSimLess - mean(powerSimLess);
+        dxSimLess = (eSimPowerLess(2) - eSimPowerLess(1));
+        areaSimPowerL = sum(hSimPowerLess) * dxSimLess ;
         
         hPowerMore = dataCountsMore.hPower;
         ePowerMore = dataCountsMore.ePower;
         powerDataMore = dataTSCMore.power.Data;
         powerDataMore = powerDataMore - mean(powerDataMore);
-        areaPowerM = sum(hPowerMore) * (ePowerMore(2) -ePowerMore(1));
+        dxMore = (ePowerMore(2)-ePowerMore(1));
+        areaPowerM = sum(hPowerMore) * dxMore;
         
-        xx = linspace(-1,1, kernelResolution);
+        hSimPowerMore = simCountsMore.hPower;
+        eSimPowerMore = simCountsMore.ePower;
+        powerSimMore = simTSCMore.simPower.Data;
+        powerSimMore = powerSimMore - mean(powerSimMore);
+        dxSimMore = (eSimPowerMore(2) - eSimPowerMore(1));
+        areaSimPowerM = sum(hSimPowerMore) * dxSimMore ;
+        
+        xmin = min([min(ePowerLess),min(eSimPowerLess),min(ePowerMore),...
+            min(eSimPowerMore)]);
+        xmax = max([max(ePowerLess),max(eSimPowerLess),max(ePowerMore), ...
+            max(eSimPowerMore)]);
+        xx = linspace(xmin,xmax, kernelResolution);
         [fPowerL, xiPowerL, bwPowerL] = ksdensity(powerDataLess,xx);
+        [fSimPowerL, xiSimPowerL, bwSimPowerL] = ksdensity(powerSimLess,xx);
         [fPowerM, xiPowerM, bwPowerM] = ksdensity(powerDataMore,xx);
+        [fSimPowerM, xiSimPowerM, bwSimPowerM] = ksdensity(powerSimMore,xx);
         areafPowerL = trapz(xx,fPowerL);
+        areafSimPowerL = trapz(xx,fSimPowerL);
         areafPowerM = trapz(xx,fPowerM);
-        fCountsPowerL = (areaPowerL/areafPowerL) .*fPowerL;
-        fCountsPowerM = (areaPowerM/areafPowerM) .*fPowerM;
+        areafSimPowerM = trapz(xx,fSimPowerM);
+        fCountsPowerL = (areaPowerL/areafPowerL).*fPowerL;
+        fCountsSimPowerL = (areaSimPowerL/areafSimPowerL).*fSimPowerL;
+        fCountsPowerM = (areaPowerM/areafPowerM).*fPowerM;
+        fCountsSimPowerM = (areaSimPowerM/areafSimPowerM).*fSimPowerM;
         pdfPowerL = normalize(fCountsPowerL);
         pdfPowerM = normalize(fCountsPowerM);
+        pdfSimPowerL = normalize(fCountsSimPowerL);
+        pdfSimPowerM = normalize(fCountsSimPowerM);
+%         KLPower(i) = kldiv(xiPowerL, pdfPowerL, pdfPowerM, 'sym');
         
         figure('units','normalized','outerposition',[0 0 1 1]);
+        subplot(2,1,1);
         hold on;
         barX = ePowerLess(2:end);
-        barY = [hPowerLess', hPowerMore'];
+        barY = [hPowerLess', hSimPowerLess'];
         bar(barX,barY,'hist');
         plot(xx,fCountsPowerL,'b-');
-        plot(xx, fCountsPowerM,'k-');        
-        legend('Hist Counts','fCountsPowerL','fCountsPowerM');
-        title(strcat('Power f = ',num2str(f(i)), ' Hz'));
+        plot(xx,fCountsSimPowerL,'k-');
+        xlim([-1.1 2.3]);
+        xlabel('Power [V^2]');
+        ylabel('Counts');
+        legend('Data Hist','Simulation Hist','Fitted Data Hist','Fitted Simulation Hist');
+        title(strcat('Smoothing 40000 samples for input signal of',' ',num2str(f(i)), ' ','Hz'));
         
-        KLPower(i) = kldiv(xiPowerL, pdfPowerL, pdfPowerM,'sym');
-       
+        subplot(2,1,2);
+        hold on;
+        barX = ePowerMore(2:end);
+        barY = [hPowerMore', hSimPowerMore'];
+        bar(barX,barY,'hist');
+        plot(xx, fCountsPowerM,'b-');
+        plot(xx, fCountsSimPowerM,'k-'); 
+        xlim([-1.1 2.3]);
+        xlabel('Power [V^2]');
+        ylabel('Counts');
+        legend('Data Hist','Simulation Hist','Fitted Data Hist','Fitted Simulation Hist');
+        title(strcat('Smoothing 40000 samples for input signal of ',num2str(f(i)), ' Hz'));
+        
+        saveas(gcf,strcat(saveFolder,'ksdensityMore_freq',num2str((i-1)),'.png'));
     end
-
-KLDiv.Vout = KLVout;
-KLDiv.Power = KLPower;
-KLDiv.Units = 'bits';
-
-Errors.KLDiv = KLDiv;
-Errors.freq = f;
-Errors.bins = bins;
-
-save(errorsFileName,'Errors','-v7.3');    
-end
