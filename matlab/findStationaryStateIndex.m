e195717eee96b7cab1c513d143d50e6850a3bd3d
joseogalde <jose.ogalde@ing.uchile.cl>
2016-06-06 12:31:21 -0400

    corregido el metodo para detectar el estado estacionario de una serie de tiempo4

--- matlab/findStationaryStateIndex.m
+++ matlab/findStationaryStateIndex.m
@@ -1,10 +1,11 @@
-function index = findStationaryStateIndex( ref, x, minimunIndex, tolerance )
+function index = findStationaryStateIndex( ref, x, minimunIndex, tolerance, span )
 % A good value for tolerance in vin is 5e-5
 instAvg = cummean(x,1)';
-instAvg = instAvg(minimunIndex : end);
-candidates = abs(instAvg - ref) < tolerance;
-indexes = find(candidates == 1);
-index = (minimunIndex - 1) +  indexes(1);
-
+smoothAvg = smooth(instAvg, span);
+smoothAvg = smoothAvg(minimunIndex : end);
+candidates = abs(smoothAvg - ref) < tolerance;
+zeroCross = find(candidates, 10, 'first');
+[~, findIndex] = min(abs(zeroCross - mean(zeroCross)));
+index = zeroCross(findIndex+1);
 end
 
