6e609f47a74ffc81438ebd3a145894894c657fa0
joseogalde <jose.ogalde@ing.uchile.cl>
2016-06-06 15:17:07 -0400

    idem al anterior

--- matlab/findStationaryStateIndex.m
+++ matlab/findStationaryStateIndex.m
@@ -1,9 +1,9 @@
 function [index, stateSignal] = findStationaryStateIndex( refLevel, signal, minimunIndex, tolerance, span )
 
-stateSignal = cummean(signal, 1)';
-smoothedSignal = smooth(stateSignal, span);
-smoothedSignal = smoothedSignal(minimunIndex : end);
-indexCandidates = abs(smoothedSignal - refLevel) < tolerance;
+accumulatedMean = cummean(signal, 1)';
+stateSignal = smooth(accumulatedMean, span);
+stateSignal = stateSignal(minimunIndex : end);
+indexCandidates = abs(stateSignal - refLevel) < tolerance;
 zeroCrosses = find(indexCandidates, 10, 'first');
 [~, findIndex] = min(abs(zeroCrosses - mean(zeroCrosses)));
 index = zeroCrosses(findIndex+1);
